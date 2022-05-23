b9e879433ef58109290a8f6d69db32dbd4e405db
Emil <emil.pedersen@refined.com>
2022-05-23 17:57:09 +0200

    new title

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,7 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-      <h1>Updating v.1</h1>
+      <h1>Updating v.2 New Ci/cd</h1>
     </nav>
     <router-view/>
   </div>
